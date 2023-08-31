8cec29d6fd012ff50bf1c2d92df678abdae69716
syahmi-najmi <76910324+syahmi-najmi@users.noreply.github.com>
2023-09-01 00:45:09 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,15 @@
+import random
+
+def guess(x):
+    random_number = random.randint(1, x)
+    guess = 0
+    while guess != random_number:
+        guess = int(input(f'Guess a number between 1 and {x}: '))
+        if guess < random_number:
+            print('Sorry, guess again. Too low.')
+        elif guess > random_number:
+            print('Sorry, guess again. Too High.')
+    
+    print(f'Yay, Congrats. You have guessed the number {random_number} correctly!!')
+
+guess(10)
